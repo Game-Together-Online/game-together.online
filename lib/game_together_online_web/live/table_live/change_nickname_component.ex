aa2d10e72bdc35f56eaf852d894430af61c905d5
Ryan Morris <mryan.morris@gmail.com>
2022-11-26 07:58:19 -0600

    feat(lobby): allow players to change their nickname

--- lib/game_together_online_web/live/table_live/change_nickname_component.ex
+++ lib/game_together_online_web/live/table_live/change_nickname_component.ex
@@ -1,6 +1,8 @@
 defmodule GameTogetherOnlineWeb.TableLive.ChangeNicknameComponent do
   use GameTogetherOnlineWeb, :live_component
 
+  alias GameTogetherOnline.Players
+
   @impl true
   def render(assigns) do
     ~H"""
@@ -11,13 +13,13 @@ defmodule GameTogetherOnlineWeb.TableLive.ChangeNicknameComponent do
 
       <.simple_form
         :let={f}
-        for={:whatever}
+        for={@changeset}
         id="change-nickname-form"
         phx-target={@myself}
         phx-change="validate"
         phx-submit="save"
       >
-        <.input field={{f, :status}} type="text" />
+        <.input field={{f, :nickname}} type="text" />
 
         <:actions>
           <.button phx-disable-with="Saving...">Save</.button>
@@ -25,5 +27,39 @@ defmodule GameTogetherOnlineWeb.TableLive.ChangeNicknameComponent do
       </.simple_form>
     </div>
     """
+  end
+
+  @impl true
+  def update(%{player: player} = assigns, socket) do
+    changeset = Players.change_player(player)
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(:changeset, changeset)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"player" => player_params}, socket) do
+    changeset =
+      socket.assigns.player
+      |> Players.change_player(player_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  @impl true
+  def handle_event("save", %{"player" => player_params}, socket) do
+    case Players.update_player(socket.assigns.player, player_params) do
+      {:ok, _player} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "Nickname updated successfully")
+         |> push_navigate(to: socket.assigns.navigate)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
   end
 end
--- test/game_together_online/players_test.exs
+++ test/game_together_online/players_test.exs
@@ -4,6 +4,7 @@ defmodule GameTogetherOnline.PlayersTest do
   alias GameTogetherOnline.Players
   alias GameTogetherOnline.Players.Player
   alias GameTogetherOnline.Administration.PlayersFixtures
+  alias GameTogetherOnline.Administration
 
   test "create_player/1 with valid data creates a role" do
     valid_attrs = %{nickname: "some nickname"}
@@ -14,6 +15,29 @@ defmodule GameTogetherOnline.PlayersTest do
 
   test "create_player/1 with invalid data returns error changeset" do
     assert {:error, %Ecto.Changeset{}} = Players.create_player(%{})
+  end
+
+  test "update_player/2 with valid data updates the player" do
+    player = PlayersFixtures.player_fixture()
+    update_attrs = %{nickname: "some updated nickname"}
+
+    {:ok, updated_player} = Players.update_player(player, update_attrs)
+
+    assert updated_player.nickname == "some updated nickname"
+    assert updated_player.id == player.id
+  end
+
+  test "update_player/2 with invalid data returns error changeset" do
+    player = PlayersFixtures.player_fixture()
+    assert {:error, %Ecto.Changeset{}} = Players.update_player(player, %{nickname: ""})
+    assert player == Administration.Players.get_player!(player.id)
+  end
+
+  test "change_player/1 returns a player changeset" do
+    valid_attrs = %{nickname: "some nickname"}
+
+    assert {:ok, %Player{} = player} = Players.create_player(valid_attrs)
+    assert %Ecto.Changeset{} = Players.change_player(player)
   end
 
   describe "get_player_by_session_token/1" do
