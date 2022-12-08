5a24acaea1177aeb442df96403240650f36048f9
Ryan Morris <mryan.morris@gmail.com>
2022-12-08 09:59:12 -0600

    Revert "feat(administration): scaffold out a view for the danger zone :D"
    This reverts commit fd9ae672288bc0c8f46e943d99cf413ea646d443.

--- lib/game_together_online_web/controllers/administration/danger_zone_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule GameTogetherOnlineWeb.Administration.DangerZoneController do
-  use GameTogetherOnlineWeb, :controller
-
-  def home(conn, _params) do
-    render(conn, :home)
-  end
-end
--- test/game_together_online_web/controllers/administration/danger_zone_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GameTogetherOnlineWeb.Administration.DangerZoneControllerTest do
-  use GameTogetherOnlineWeb.ConnCase
-
-  test "GET /danger_zone", %{conn: conn} do
-    conn = get(conn, ~p"/administration/danger_zone")
-    assert html_response(conn, 200) =~ "THE DANGER ZONE"
-  end
-end
