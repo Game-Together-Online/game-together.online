de579814880f94c6222894a9f0d79cc68a445e18
Ryan Morris <mryan.morris@gmail.com>
2022-12-18 07:32:28 -0600

    feat(tables): move the current tab to the URL

--- lib/game_together_online_web/live/table_live/index.ex
+++ lib/game_together_online_web/live/table_live/index.ex
@@ -4,6 +4,8 @@ defmodule GameTogetherOnlineWeb.TableLive.Index do
   alias GameTogetherOnlineWeb.TableLive.IndexHTML
   alias GameTogetherOnline.Tables
 
+  @default_tab "chat"
+
   def mount(%{"id" => id}, _session, socket) do
     table = Tables.get_table!(id)
 
@@ -19,14 +21,21 @@ defmodule GameTogetherOnlineWeb.TableLive.Index do
   end
 
   def handle_params(params, url, socket) do
+    current_tab = Map.get(params, "tab", @default_tab)
+
     {:noreply,
      socket
      |> assign(invite_url: url)
+     |> assign(current_tab: current_tab)
      |> assign(show_edit_nickname_modal: show_edit_nickname?(params))}
   end
 
   def handle_info(table, socket) do
     {:noreply, assign(socket, table: table)}
+  end
+
+  def handle_event("select_tab", %{"tab" => tab}, socket) do
+    {:noreply, push_patch(socket, to: ~p"/tables/#{socket.assigns.table.id}/lobby?tab=#{tab}")}
   end
 
   def render(%{live_action: :lobby} = assigns), do: IndexHTML.lobby(assigns)
