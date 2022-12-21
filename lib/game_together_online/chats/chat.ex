9d47567c9d6eb1e030ded7be7d87043bab62bc9c
Ryan Morris <mryan.morris@gmail.com>
2022-12-21 08:16:56 -0600

    feat(chats): create a chat for each new table

---
+++ lib/game_together_online/chats/chat.ex
@@ -0,0 +1,22 @@
+defmodule GameTogetherOnline.Chats.Chat do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  alias GameTogetherOnline.Administration.Tables.Table
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "chats" do
+    belongs_to :table, Table
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(chat, attrs) do
+    chat
+    |> cast(attrs, [:table_id])
+    |> validate_required([:table_id])
+    |> foreign_key_constraint(:table_id)
+  end
+end
