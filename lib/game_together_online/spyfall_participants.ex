fd21a13bd3d76f28fa7e8e3b9653c425a51ae595
Ryan Morris <mryan.morris@gmail.com>
2023-01-04 08:02:57 -0600

    feat(spyfall): add the ability to leave the game

--- lib/game_together_online/spyfall_participants.ex
+++ lib/game_together_online/spyfall_participants.ex
@@ -1,6 +1,18 @@
 defmodule GameTogetherOnline.SpyfallParticipants do
+  import Ecto.Query
+
   alias GameTogetherOnline.SpyfallParticipants.SpyfallParticipant
   alias GameTogetherOnline.Repo
+
+  def delete_spyfall_participant_by_game_and_player(game_id, player_id) do
+    SpyfallParticipant
+    |> where(
+      [spyfall_participant],
+      spyfall_participant.player_id == ^player_id and
+        spyfall_participant.spyfall_game_id == ^game_id
+    )
+    |> Repo.delete_all()
+  end
 
   def create_spyfall_participant(attrs \\ %{}) do
     %SpyfallParticipant{}
