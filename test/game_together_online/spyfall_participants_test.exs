84c2c6e6376a3c99424527ee98a85e1b553b8895
Ryan Morris <mryan.morris@gmail.com>
2023-01-07 08:51:04 -0600

    feat(spyfall): add the ability to mark yourself as ready and unready to start a game

--- test/game_together_online/spyfall_participants_test.exs
+++ test/game_together_online/spyfall_participants_test.exs
@@ -13,6 +13,27 @@ defmodule GameTogetherOnline.SpyfallParticipantsTest do
 
     @invalid_attrs %{ready_to_start: nil}
 
+    test "toggle_readiness/1 toggles the readiness for the participant" do
+      player = PlayersFixtures.player_fixture()
+      table = TablesFixtures.table_fixture()
+      spyfall_game = SpyfallGamesFixtures.spyfall_game_fixture(%{table_id: table.id})
+
+      valid_attrs = %{
+        ready_to_start: true,
+        player_id: player.id,
+        spyfall_game_id: spyfall_game.id
+      }
+
+      assert {:ok, %SpyfallParticipant{} = spyfall_participant} =
+               SpyfallParticipants.create_spyfall_participant(valid_attrs)
+
+      {:ok, spyfall_participant} = SpyfallParticipants.toggle_readiness(spyfall_participant)
+      refute spyfall_participant.ready_to_start
+
+      {:ok, spyfall_participant} = SpyfallParticipants.toggle_readiness(spyfall_participant)
+      assert spyfall_participant.ready_to_start
+    end
+
     test "delete_spyfall_participant_by_game_and_player/2 deletes any matching participants" do
       player = PlayersFixtures.player_fixture()
       table = TablesFixtures.table_fixture()
