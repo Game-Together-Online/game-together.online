0293eef5ce1a348d86a9bb4af0b65af250861391
Ryan Morris <mryan.morris@gmail.com>
2023-01-07 18:44:08 -0600

    feat(spyfall participants): add the UI for showing spyfall participants

--- lib/game_together_online_web/components/spyfall/lobby.ex
+++ lib/game_together_online_web/components/spyfall/lobby.ex
@@ -52,78 +52,283 @@ defmodule GameTogetherOnlineWeb.Components.Spyfall.Lobby do
       |> set_current_player_ready_to_play()
 
     ~H"""
-    <div>
-      <.live_component
-        id={"lobby-header-for-table-#{@table.id}"}
-        module={LobbyHeader}
-        current_player={@current_player}
-        current_tab={@current_tab}
-        table={@table}
-      >
-        <:actions>
-          <.link
-            patch={~p"/tables/#{@table.id}/lobby?tab=#{@current_tab}&edit_nickname=true"}
-            class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-          >
-            Change Your Nickname
-          </.link>
-        </:actions>
-      </.live_component>
-      <div class="flex mt-8 flex-col md:flex-row space-y-4">
-        <div class="overflow-hidden rounded-lg bg-gray-50 w-full md:w-96">
-          <div class="px-4 py-5 sm:p-6">
-            <h2 id="summary-heading" class="text-lg font-medium text-gray-900">Game Settings</h2>
+    <div class="p-4 grow">
+      <div class="flex flex-col h-full">
+        <.live_component
+          id={"lobby-header-for-table-#{@table.id}"}
+          module={LobbyHeader}
+          current_player={@current_player}
+          current_tab={@current_tab}
+          table={@table}
+        >
+          <:actions>
+            <.link
+              patch={~p"/tables/#{@table.id}/lobby?tab=#{@current_tab}&edit_nickname=true"}
+              class="inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+            >
+              Change Your Nickname
+            </.link>
+          </:actions>
+        </.live_component>
 
-            <dl class="mt-6 space-y-4">
-              <div class="flex items-center justify-between">
-                <dt class="text-sm text-gray-600">Round Time Limit</dt>
-                <dd class="text-sm font-medium text-gray-900">8 Minutes</dd>
-              </div>
-            </dl>
+        <div class="flex mt-8 flex-col md:flex-row h-full">
+          <div class="overflow-hidden w-full md:w-96 flex flex-col justify-between">
+            <div class="px-4 py-5 sm:p-6 rounded-lg bg-gray-50">
+              <h2 id="summary-heading" class="text-lg font-medium text-gray-900">Game Settings</h2>
 
-            <div class="mt-6 space-y-2">
-              <%= if @current_player_has_joined do %>
-                <div class="flex flex-row justify-end">
-                  <div class="flex h-5 items-center">
-                    <input
-                      id="comments"
-                      phx-click="toggle_ready_to_play"
-                      phx-target={@myself}
-                      aria-describedby="comments-description"
-                      name="comments"
-                      type="checkbox"
-                      checked={@current_player_ready_to_play}
-                      class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
-                    />
-                  </div>
-                  <div class="ml-3 text-sm">
-                    <label for="comments" class="font-medium text-gray-700">Ready To Start</label>
-                  </div>
+              <dl class="mt-6 space-y-4">
+                <div class="flex items-center justify-between">
+                  <dt class="text-sm text-gray-600">Round Time Limit</dt>
+                  <dd class="text-sm font-medium text-gray-900">8 Minutes</dd>
                 </div>
-                <button
-                  phx-click="remove_user_from_game"
-                  phx-target={@myself}
-                  type="button"
-                  class="w-full items-center rounded-md border border-transparent bg-indigo-100 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
-                >
-                  Leave The Game
-                </button>
-              <% else %>
-                <button
-                  type="button"
-                  phx-click="add_user_to_game"
-                  phx-target={@myself}
-                  class="w-full rounded-md border border-transparent bg-indigo-600 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
-                >
-                  Join The Game
-                </button>
-              <% end %>
+              </dl>
+
+              <div class="mt-6 space-y-2">
+                <%= if @current_player_has_joined do %>
+                  <div class="flex flex-row justify-end">
+                    <div class="flex h-5 items-center">
+                      <input
+                        id="comments"
+                        phx-click="toggle_ready_to_play"
+                        phx-target={@myself}
+                        aria-describedby="comments-description"
+                        name="comments"
+                        type="checkbox"
+                        checked={@current_player_ready_to_play}
+                        class="h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-500"
+                      />
+                    </div>
+                    <div class="ml-3 text-sm">
+                      <label for="comments" class="font-medium text-gray-700">Ready To Start</label>
+                    </div>
+                  </div>
+                  <button
+                    phx-click="remove_user_from_game"
+                    phx-target={@myself}
+                    type="button"
+                    class="w-full items-center rounded-md border border-transparent bg-indigo-100 py-3 text-base font-medium text-indigo-700 hover:bg-indigo-200 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                  >
+                    Leave The Game
+                  </button>
+                <% else %>
+                  <button
+                    type="button"
+                    phx-click="add_user_to_game"
+                    phx-target={@myself}
+                    class="w-full rounded-md border border-transparent bg-indigo-600 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-50"
+                  >
+                    Join The Game
+                  </button>
+                <% end %>
+              </div>
             </div>
           </div>
-        </div>
 
-        <div class="hidden md:block">
-          SPYFALL! <%= @table.id %>
+          <div class="flex flex-col px-4 grow">
+            SPYFALL! <%= @table.id %>
+            <ul role="list" class="grow divide-y divide-gray-200 overflow-y-scroll flex-grow h-0">
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Calvin Hawkins</p>
+                  <p class="text-sm text-gray-500">calvin.hawkins@example.com</p>
+                </div>
+              </li>
+
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Kristen Ramos</p>
+                  <p class="text-sm text-gray-500">kristen.ramos@example.com</p>
+                </div>
+              </li>
+
+              <li class="flex py-4">
+                <img
+                  class="h-10 w-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                  alt=""
+                />
+                <div class="ml-3">
+                  <p class="text-sm font-medium text-gray-900">Ted Fox</p>
+                  <p class="text-sm text-gray-500">ted.fox@example.com</p>
+                </div>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
